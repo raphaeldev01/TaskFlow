b5c551fd26909175819316095d2038798ed3d9ac
Raphael Developer <raphael.developer@hotmail.com>
2025-12-02 01:15:26 -0300

    feat: login user router was created

--- src/Routers/UserAuth.ts
+++ src/Routers/UserAuth.ts
@@ -20,4 +20,19 @@ userAuth.post("/create", async (req, res) => {
     res.send(response)
 })
 
+userAuth.post("/login", async (req, res) => {
+    if(!req.body) return res.status(400).json({error: true, message: "invalid body"})
+    
+    const {user, password} = req.body;
+
+    if(!user) return res.status(400).json({error: true, message: "Body don't have the user"})
+    if(!password) return res.status(400).json({error: true, message: "Body don't have the password"})
+    
+    const response = await LoginUser(user, password)
+
+    if(response.error) return res.status(401).json(response)
+    
+    res.send(response)
+})
+
 export default userAuth
